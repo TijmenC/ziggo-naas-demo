ff94092bce6a7aeb6f98dd5bb880ad7b348c826f
Coenders,Tijmen T.C.P <70951009+TijmenC@users.noreply.github.com>
2023-10-05 15:34:32 +0200

    Initial commit

---
+++ src/main/java/com/naasdemo/ziggonaasdemo/ZiggoNaasDemoApplication.java
@@ -0,0 +1,13 @@
+package com.naasdemo.ziggonaasdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiggoNaasDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZiggoNaasDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naasdemo/ziggonaasdemo/ZiggoNaasDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naasdemo.ziggonaasdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiggoNaasDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
